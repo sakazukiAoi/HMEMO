41903f9a84f31461b162929fd7fa1eda0f514e93
sakazukiAoi <52361879+sakazukiAoi@users.noreply.github.com>
2024-08-18 23:56:51 +0900

    Add files via upload

---
+++ button.js
@@ -0,0 +1,17 @@
+function changeColor(boxId, colorClass) {
+    const box = document.getElementById(boxId);
+    box.classList.toggle(colorClass);
+}
+/*
+const button2 = document.getElementById("box2");
+function changeColoror(){
+button2.classList.toggle("orange");
+}
+const button3 = document.getElementById("box3");
+function changeColorgr(){
+button3.classList.toggle("gr");
+}
+const button4 = document.getElementById("box4");
+function changeColorlb(){
+button4.classList.toggle("lb");
+}*/
